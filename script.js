eb6b0f86e29eb85473f99d25b9c7206d3f6c3a6f
LuKa44x <lucavicari44@gmail.com>
2025-05-16 23:43:32 +0200

    Started styling to test the new js script
    Added getBookElement fun to create the dom element of every book to manipulate them in case
    Added also a button in the dom, for every book, ready to give it some remove script in the next commit.

--- script.js
+++ script.js
@@ -8,17 +8,35 @@ function Book(id, title, author, pages, read, description) {
   this.read = read;
   this.description = description;
 }
-
-function addBookToLibrary(){
-    const book = new Book(getBookID(),title.value, author.value, pages.value, read.value, description.value);
-    myLibrary.push(book);
-}
+const gridContainer = document.querySelector(".gridContainer");
 let title = document.querySelector("#title");
-console.log(title.value);
 let author = document.querySelector("#author");
 let pages = document.querySelector("#pages");
 let read = document.querySelector("#read");
 let description = document.querySelector("#description");
+
+function addBookToLibrary(){
+    let bookID = getRandomBookID();
+    const bookObj = new Book(bookID, title.value, author.value, pages.value, read.value, description.value);
+    myLibrary.push(bookObj);
+    const book = getBookElement(bookObj, bookID);
+    gridContainer.appendChild(book);
+}
+
+function getBookElement(book, bookID){
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+    bookElement.id = bookID;
+    bookElement.innerHTML =`
+        <h3>${book.title}</h3>
+        <p>Autore: ${book.author}</p>
+        <p>Pagine: ${book.pages}</p>
+        <p>Letto: ${book.read}</p>
+        <p>${book.description}</p>
+        <button class=${"bookBtn"} id=${bookID}>Remove</button>
+        `;
+        return bookElement;
+}
 
 function getRandomBookID(){
     return self.crypto.randomUUID();
